4cea18d8af7f071dcad6f85638434918ea4fc463
user <user@WIN-3VBHTKR11U0>
2019-06-22 19:00:46 +0300

    jjj

--- AngularTrain/Angular1/src/app/app.component.ts
+++ AngularTrain/Angular1/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild, TemplateRef, ViewContainerRef, ElementRef, AfterViewInit, Renderer2} from '@angular/core';
-import {WhileDirective} from "./while.directive";
+import { Component, ViewChild, TemplateRef, ViewContainerRef, ElementRef, AfterViewInit, Renderer2 } from '@angular/core';
+import { WhileDirective } from "./while.directive";
 
 // @Component({
 //     selector: 'vcr',
@@ -10,29 +10,44 @@ import {WhileDirective} from "./while.directive";
 //   export class VcrComponent implements AfterViewInit{
 //     constructor() {
 //     }
-    
+
 //     ngAfterViewInit() {
 //         const buttonElement = this._renderer.createElement('button');
 //     this._vcr.createEmbeddedView(buttonElement);
 //     //   this._vcr.createEmbeddedView(this.tpl);
 //     }
 //   }
-  
-  @Component({
-    selector: 'my-app',
-    template: `
+
+@Component({
+  selector: 'my-app',
+  template: `
     <div>
-        <ng-template [while]="condition">
-                <h1>Hi from template</h1>
-            </ng-template>
+        <ul>
+          <li *ngFor="let item of list; trackBy:identify">{{item.id}} and {{item.name}}</li>
+        </ul>
     </div>
         
-        <button (click)="toggle()">Toggle</button>
+        <button (click)="updateCollection()">ToggleUpdate</button>
+        <button (click)="changeCollection()">ToggleChange</button>
     `,
-  })
-  export class AppComponent{
-    condition: boolean=false;
-    toggle(){
-        this.condition=!this.condition;
-    }
+})
+export class AppComponent {
+  list: any[];
+
+  constructor() {
+    this.list = [{ id: 1, name: 'Gustavo' }, { id: 2, name: 'Costa' }]
   }
+
+  identify(index: number, item: any) {
+    //do what ever logic you need to come up with the unique identifier of your item in loop, I will just return the object id.
+    return item.id;
+  }
+
+  updateCollection() {
+    this.list = [{ id: 1, name: 'Gustavo' }, { id: 2, name: 'Costa' }]
+  }
+
+  changeCollection() {
+    this.list = [{ id: 1, name: 'Gustavo' }, { id: 2, name: 'Costo' }]
+  }
+}
