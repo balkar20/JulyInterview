f835ed1faa65d3a268c10cc9aa5da7275354ac59
balkar20 <balkar20@mail.ru>
2019-07-31 17:22:39 +0300

    hg

--- gridTrain/main.js
+++ gridTrain/main.js
@@ -1,40 +1,95 @@
-let arr = [
-    {
-        id:1,
-        name:"Коврик м555",
-        description:"kjkjkjkjkjjnnnnn",
-        url: "model1.jpg"
-        
+let arr = [{
+        id: 1,
+        name: "м555",
+        description: "kjkjkjkjkjjnnnnn",
+        url: "model1.jpg",
+        price: 50
     },
     {
-        id:2,
-        name:"Коврик м556",
-        description:"kjkjkjkjkjjnnnnn",
-        url: "model2.jpg"
-        
+        id: 2,
+        name: "м556",
+        description: "kjkjkjkjkjjnnnnn",
+        url: "model2.jpg",
+        price: 50
+
     },
     {
-        id:3,
-        name:"Коврик м557",
-        description:"kjkjkjkjkjjnnnnn",
-        url: "model3.jpg"
-        
+        id: 3,
+        name: "м557",
+        description: "kjkjkjkjkjjnnnnn",
+        url: "model3.jpg",
+        price: 50
+
     },
     {
-        id:4,
-        name:"Коврик м558",
-        description:"kjkjkjkjkjjnnnnn",
-        url: "model4.jpg"
-        
+        id: 4,
+        name: "м558",
+        description: "kjkjkjkjkjjnnnnn",
+        url: "model4.jpg",
+        price: 50
+
+    },
+    {
+        id: 4,
+        name: "м558",
+        description: "kjkjkjkjkjjnnnnn",
+        url: "model5.jpg",
+        price: 50
+
+    },
+    {
+        id: 4,
+        name: "м558",
+        description: "kjkjkjkjkjjnnnnn",
+        url: "model6.jpg",
+        price: 50
+
+    },
+    {
+        id: 4,
+        name: "м558",
+        description: "kjkjkjkjkjjnnnnn",
+        url: "model7.jpg",
+        price: 50
+
+    },
+    {
+        id: 4,
+        name: "м558",
+        description: "kjkjkjkjkjjnnnnn",
+        url: "model8.jpg",
+        price: 50
+
+    },
+    {
+        id: 4,
+        name: "м558",
+        description: "kjkjkjkjkjjnnnnn",
+        url: "model9.jpg",
+        price: 50
+
+    },
+    {
+        id: 4,
+        name: "м558",
+        description: "kjkjkjkjkjjnnnnn",
+        url: "model10.jpg",
+        price: 50
+
     },
 ];
 
 let EllArr = [];
 arr.forEach((el) => {
     var img = $(`<img src=${el.url}>`).addClass("frameImg");
+    var info = $(`
+    <div class="info">
+        <span class="model">Модель: <span class="val">${el.name}</span></span>
+        <span class="price">Цена: <span class="val">${el.price}</span></span>
+        <a href="#" class="more">Подробнее..</a>
+    </div>`);
     var domEl = $(`
     <div>
-    </div>`).addClass("frame").append(img);
+    </div>`).addClass("frame").append(img).append(info);
     $(".container").append(domEl);
 });
-
