4854c8b05d60f8bf99602204b6417e46f5419bd3
user <user@WIN-3VBHTKR11U0>
2019-07-31 21:13:07 +0300

    jj

--- gridTrain/main.js
+++ gridTrain/main.js
@@ -33,7 +33,7 @@ let arr = [{
         id: 4,
         name: "м558",
         description: "kjkjkjkjkjjnnnnn",
-        url: "model5.jpg",
+        url: "http://www.autowaysgroup.com/Content/ue/net/upload1/Other/110410/6361871073512862882090563.png",
         price: 50
 
     },
@@ -41,7 +41,7 @@ let arr = [{
         id: 4,
         name: "м558",
         description: "kjkjkjkjkjjnnnnn",
-        url: "model6.jpg",
+        url: "http://dk.autowaysgroup.com/Content/ue/net/upload1/Other/110410/6361869264611934511679960.png",
         price: 50
 
     },
@@ -49,7 +49,7 @@ let arr = [{
         id: 4,
         name: "м558",
         description: "kjkjkjkjkjjnnnnn",
-        url: "model7.jpg",
+        url: "http://dk.autowaysgroup.com/Content/ue/net/upload1/Other/110410/6361871061958608849663153.png",
         price: 50
 
     },
@@ -57,7 +57,7 @@ let arr = [{
         id: 4,
         name: "м558",
         description: "kjkjkjkjkjjnnnnn",
-        url: "model8.jpg",
+        url: "https://refaccionariamario.info/5374-large_default/juego-de-tapetes-de-alfombra-con-hule-con-emblema-rojo-de-vw.jpg",
         price: 50
 
     },
@@ -65,7 +65,7 @@ let arr = [{
         id: 4,
         name: "м558",
         description: "kjkjkjkjkjjnnnnn",
-        url: "model9.jpg",
+        url: "https://s1.medias-norauto.es/images_produits/alfombrillas-pvc-cancun/650x650/4-alfombrillas-coche-universales-de-pvc-cancun-color-carbono--761041.jpg",
         price: 50
 
     },
@@ -73,7 +73,7 @@ let arr = [{
         id: 4,
         name: "м558",
         description: "kjkjkjkjkjjnnnnn",
-        url: "model10.jpg",
+        url: "https://www.google.com/search?q=%D1%84%D0%BE%D1%82%D0%BE+%D0%BA%D0%BE%D0%B2%D1%80%D0%BE%D0%B2+%D0%B4%D0%BB%D1%8F+%D0%B0%D0%B2%D1%82%D0%BE&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi3kZHYxt_jAhXDtYsKHfteAPIQ_AUIESgB&biw=1280&bih=625#imgdii=EyD-X6mHF8dSPM:&imgrc=-E5lSgBkI3G_EM:",
         price: 50
 
     },
@@ -81,6 +81,7 @@ let arr = [{
 
 let EllArr = [];
 arr.forEach((el) => {
+    
     var img = $(`<img src=${el.url}>`).addClass("frameImg");
     var info = $(`
     <div class="info">
