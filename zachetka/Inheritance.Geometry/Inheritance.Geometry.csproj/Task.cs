2cf3f823d081e2fd1e9dfeb3568761e72ab98b38
balkar20 <balkar20@mail.ru>
2019-09-17 17:15:59 +0300

    hjuj

--- zachetka/Inheritance.Geometry/Inheritance.Geometry.csproj/Task.cs
+++ zachetka/Inheritance.Geometry/Inheritance.Geometry.csproj/Task.cs
@@ -6,47 +6,77 @@ using System.Threading.Tasks;
 
 namespace Inheritance.Geometry
 {
-	public abstract class Body
-	{
-	    public abstract double GetVolume();
-	}
+    public interface IVisitor
+    {
+        void VisitBody(Body elem);
+    }
+    public abstract class Body
+    {
+        public abstract double GetVolume();
+        public abstract void Accept(IVisitor visitor);
 
-	public class Ball : Body
-	{
-		public double Radius { get; set; }
-	    public override double GetVolume()
-	    {
-	        return 4.0 * Math.PI * Radius * Radius * Radius / 3;
+    }
+
+    public class Ball : Body
+    {
+        public double Radius { get; set; }
+        public override double GetVolume()
+        {
+            return 4.0 * Math.PI * Radius * Radius * Radius / 3;
         }
-	}
 
-	public class Cube : Body
-	{
-		public double Size { get; set; }
-	    public override double GetVolume()
-	    {
-	        return Size * Size * Size;
-        }
-	}
-
-	public class Cyllinder : Body
-	{
-		public double Height { get; set; }
-		public double Radius { get; set; }
-	    public override double GetVolume()
-	    {
-	        return Math.PI * Radius * Radius * Height;
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.VisitBody(this);
         }
     }
 
-	// Заготовка класса для задачи на Visitor
-	public class SurfaceAreaVisitor
-	{
-		public double SurfaceArea { get; private set; }
-	}
+    public class Cube : Body
+    {
+        public double Size { get; set; }
+        public override double GetVolume()
+        {
+            return Size * Size * Size;
+        }
 
-	public class DimensionsVisitor
-	{
-		public Dimensions Dimensions { get; private set; }
-	}
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.VisitBody(this);
+        }
+    }
+
+    public class Cyllinder : Body
+    {
+        public double Height { get; set; }
+        public double Radius { get; set; }
+        public override double GetVolume()
+        {
+            return Math.PI * Radius * Radius * Height;
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.VisitBody(this);
+        }
+    }
+
+    // Заготовка класса для задачи на IVisitor
+    public class SurfaceAreaVisitor: IVisitor
+    {
+        public double SurfaceArea { get; private set; }
+        public void VisitBody(Body elem)
+        {
+            elem.Accept(this);
+        }
+    }
+
+    public class DimensionsVisitor : IVisitor
+    {
+        public Dimensions Dimensions { get; private set; }
+        public void VisitBody(Body elem)
+        {
+            DimensionsVisitor
+            elem.Accept(this);
+        }
+    }
 }
