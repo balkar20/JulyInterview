fcc166d82e93f4b23c2aeed1173f1e2c495db61e
balkar20 <balkar20@mail.ru>
2019-09-18 17:36:00 +0300

    table compiled

--- zachetka/GenericsTables/Generics.Tables.csproj/Table.cs
+++ zachetka/GenericsTables/Generics.Tables.csproj/Table.cs
@@ -6,5 +6,46 @@ using System.Threading.Tasks;
 
 namespace Generics.Tables
 {
-    
+    public class Table<TRow, TColumn, TVal> 
+    {
+        public TRow[] Rows { get; set; }
+        public TColumn[] Columns { get; set; }
+
+        public TableWorker<TRow, TColumn, TVal> Open { get; set; }
+        public TableWorker<TRow, TColumn, TVal> Existed { get; set; }
+
+        
+
+        public void AddRow(TRow row)
+        {
+
+        }
+
+        public void AddColumn(TColumn column)
+        {
+
+        }
+
+        public TVal GetData(TRow row, TColumn column)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class TableWorker<TR, TC, TVal>
+    {
+        public Table<TR, TC, TVal> Table { get; set; }
+
+        public TVal this[TR row, TC column]
+        {
+            get
+            {
+                return Table.GetData(row, column);
+            }
+            set
+            {
+                
+            }
+        }
+    }
 }
