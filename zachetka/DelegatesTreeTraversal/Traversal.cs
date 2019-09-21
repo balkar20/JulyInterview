63275a45a3cef20fc6d87067b07a5613a2edef24
user <user@WIN-3VBHTKR11U0>
2019-09-21 18:42:00 +0300

    jhghj

--- zachetka/DelegatesTreeTraversal/Traversal.cs
+++ zachetka/DelegatesTreeTraversal/Traversal.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,29 @@ using System.Threading.Tasks;
 
 namespace Delegates.TreeTraversal
 {
-   
+    public class Traversal
+    {
+        public static IEnumerable<T> GetBinaryTreeValues<T>(BinaryTree<T> data)
+        {
+            T root = data.Value;
+            return new List<T>();
+        }
+
+        public static IEnumerable<T> GetEndJobs<T>(T data)
+        {
+            return new List<T>();
+        }
+
+        public static IEnumerable<Product> GetProducts<T>(T data) //where T : ProductCategory
+        {
+            return new List<Product>();
+        }
+
+        private static IEnumerable<T> GetTreeValues<T, TIntermediate>(T data, Func<T,TIntermediate> func)
+        {
+            var result = new List<T>();
+            T root = data;
+            return result;
+        }
+    }
 }
