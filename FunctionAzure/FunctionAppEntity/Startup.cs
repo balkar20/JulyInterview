80caf16ed08f7355df171181165816169299d229
balkar20 <balkar20@mail.ru>
2019-10-24 17:02:23 +0300

    hj

---
+++ FunctionAzure/FunctionAppEntity/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(FunctionAppEntity.Startup))]
+namespace FunctionAppEntity
+{
+    public class Startup: FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("SqlConnectionString");
+            builder.Services.AddDbContext<UserContext>(
+                options => SqlServerDbContextOptionsExtensions.UseSqlServer(options, connectionString));
+        }
+    }
+}
---
+++ FunctionAzure/FunctionAppEntity/UserContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FunctionAppEntity
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } 
+        public string Password { get; set; }
+        public int Age { get; set; }
+    }
+}
