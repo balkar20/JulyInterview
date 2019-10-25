34c07db744616615a1f157a79fb3a6dd8e6109de
balkar20 <balkar20@mail.ru>
2019-10-25 15:43:33 +0300

    ш

---
+++ Patterns/Arena/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Arena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    struct MyStruct
+    {
+        public int ai;
+
+        public string name;
+
+    }
+
+    interface ISomeInterface
+    {
+        
+    }
+
+    struct MyStruct2 : ISomeInterface
+    {
+        
+    }
+}
