ea6ef7b3c184320170ac766df77cda68be390de1
user <user@WIN-3VBHTKR11U0>
2019-11-06 19:50:39 +0300

    jgjhg

---
+++ Patterns/Arena2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Arena2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            ITest test = new Test(2);
+            MethTest(test);
+            test.ShowI();
+            Console.ReadLine();
+        }
+
+        static void MethTest(ITest test)
+        {
+            test.i = 10;
+        }
+    }
+
+    public struct Test:ITest
+    {
+        public Test(int i)
+        {
+            this.i = i;
+        }
+
+        public int i { get; set; }
+
+        public void ShowI()
+        {
+            Console.WriteLine($"Test i: {this.i}");
+        }
+    }
+
+    public interface ITest
+    {
+        int i { get; set; }
+        void ShowI();
+    }
+}
---
+++ Patterns/Arena3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Arena3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a1 = Get().First();
+            var a2 = Get().ToList();
+
+            Console.WriteLine($"First : {a1}");
+            Console.WriteLine($"List : ");
+
+            foreach (var i in a2)
+            {
+                Console.WriteLine($"el: {i}");
+            }
+
+            foreach (var i in GetAuth().ToList())
+            {
+                Console.WriteLine($"el: {i}");
+            }
+            
+
+        }
+
+        static IEnumerable<int> Get()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                yield return 5;
+            }
+            //for(; ;)
+            //yield return 5;
+        }
+
+        static IEnumerable<int> GetAuth()
+        {
+            throw new Exception("xxx");
+            yield return 5;
+        }
+
+
+    }
+}
