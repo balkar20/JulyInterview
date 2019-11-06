c3bf72c042ce0f3204cfc37f95f99781f5994458
balkar20 <balkar20@mail.ru>
2019-11-06 17:55:19 +0300

    kj

---
+++ newSharp/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Indexes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var indexFirst = new Index(0); // индекс первого элемента
+            var indexLast = new Index(1, fromEnd: true); // индекс последнего элемента
+            var index1 = new Index(5); // индекс 5 элемента
+            var index2 = new Index(2, fromEnd: true); // индекс 2 элемента с конца
+
+            var values = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            int v1 = values[indexFirst]; // 0
+            int v2 = values[0]; // 0
+            bool areSame1 = indexFirst.Equals(0); // true
+
+            int v3 = values[indexLast]; // 9
+            int v4 = values[^2]; // 8
+            bool areSame2 = indexLast.Equals(index2); // false
+            Console.WriteLine($"indexLast == index2 : {areSame2}");
+        }
+    }
+}
---
+++ newSharp/newSharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+#nullable enable
+
+namespace Nullables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("Vlad", "Balkarov", null);
+
+            var person2 = (null as Person)!?.FullName;
+
+            Console.WriteLine($"{person2}");
+            Console.WriteLine($"{person.FirstName} {person.FullName}");
+        }
+    }
+
+    class Person
+    {
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? MiddleName { get; set; }
+        public Person(string first, string last, string middle) =>
+          (FirstName, LastName, MiddleName) = (first, last, middle);
+        public string FullName =>
+          $"{FirstName} {MiddleName?[0]} {LastName}";
+    }
+}
