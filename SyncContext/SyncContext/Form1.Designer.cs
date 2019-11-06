1a13d9c390d183aa996b2a7f222768acd443ef76
user <user@WIN-3VBHTKR11U0>
2019-11-06 19:52:58 +0300

    kjkl

---
+++ SyncContext/SyncContext/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace SyncContext
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mListBox = new System.Windows.Forms.ListBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.mToolStripButtonThreads = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mListBox
+            // 
+            this.mListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mListBox.FormattingEnabled = true;
+            this.mListBox.Location = new System.Drawing.Point(0, 0);
+            this.mListBox.Name = "mListBox";
+            this.mListBox.Size = new System.Drawing.Size(284, 264);
+            this.mListBox.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mToolStripButtonThreads});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(284, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // mToolStripButtonThreads
+            // 
+            this.mToolStripButtonThreads.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.mToolStripButtonThreads.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.mToolStripButtonThreads.Name = "mToolStripButtonThreads";
+            this.mToolStripButtonThreads.Size = new System.Drawing.Size(148, 22);
+            this.mToolStripButtonThreads.Text = "Press Here to start threads";
+            this.mToolStripButtonThreads.Click += new System.EventHandler(this.mToolStripButtonThreads_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 264);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.mListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox mListBox;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton mToolStripButtonThreads;
+    }
+}
---
+++ SyncContext/SyncContext/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SyncContext
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mToolStripButtonThreads_Click(object sender, EventArgs e)
+        {
+            // посмотрим id потока
+            int id = Thread.CurrentThread.ManagedThreadId;
+            Trace.WriteLine("mToolStripButtonThreads_Click thread: " + id);
+
+            // захватим контекст синхронизации ассоциированный с этим
+            // потоком (UI поток), и сохраним его в uiContext
+            // отметье что этот контекст устанавливается в UI потоке
+            // во время создания формы (вне зоны вашего контроля)
+            // также отметье, что не каждый поток имеет контекст синхронизации связанный с ним.
+            SynchronizationContext uiContext = SynchronizationContext.Current;
+
+            // Создадим поток и зададим ему метод Run для исполнения
+            Thread thread = new Thread(Run);
+
+            // Запустим поток и установим ему контекст синхронизации,
+            // таким образом этот поток сможет обновлять UI
+            thread.Start(uiContext);
+        }
+
+        private void Run(object state)
+        {
+            // смотри id потока
+            int id = Thread.CurrentThread.ManagedThreadId;
+            Trace.WriteLine("Run thread: " + id);
+
+            // вытащим контекст синхронизации из state'а
+            SynchronizationContext uiContext = state as SynchronizationContext;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                // Тут мог бы быть ваш код который обращается к базе
+                // или выполняет какие-то вычисления
+                Thread.Sleep(10);
+
+                // испольуем UI контекст для обновления интерфейса, 
+                // посредством исполнения метода UpdateUI, метод UpdateUI 
+                // будет исполнен в UI потоке
+
+                uiContext.Post(UpdateUI, "line " + i.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Этот метод исполняется в основном UI потоке
+        /// </summary>
+        private void UpdateUI(object state)
+        {
+            int id = Thread.CurrentThread.ManagedThreadId;
+            Trace.WriteLine("UpdateUI thread:" + id);
+            string text = state as string;
+            mListBox.Items.Add(text);
+        }
+    }
+}
