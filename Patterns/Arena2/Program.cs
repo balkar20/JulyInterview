a715aa1ea5ef2f5523d6750948e78a77f44a8cb3
user <user@WIN-3VBHTKR11U0>
2019-11-08 23:02:20 +0300

    jh

--- Patterns/Arena2/Program.cs
+++ Patterns/Arena2/Program.cs
@@ -4,13 +4,38 @@ namespace Arena2
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
-            ITest test = new Test(2);
-            MethTest(test);
-            test.ShowI();
-            Console.ReadLine();
+            // int.MaxValue is 2,147,483,647.
+            const int ConstantMax = int.MaxValue;
+            int int1;
+            int int2;
+            int variableMax = 2147483647;
+
+            // The following statements are checked by default at compile time. They do not
+            // compile.
+            //int1 = 2147483647 + 10;
+            //int1 = ConstantMax + 10;
+
+            // To enable the assignments to int1 to compile and run, place them inside 
+            // an unchecked block or expression. The following statements compile and
+            // run.
+            unchecked
+            {
+                int1 = 2147483647 + 10;
+            }
+
+            checked
+            {
+                int1 = 2147483647;
+            }
+            
+
+            Console.WriteLine($"{int1}");
+            //ITest test = new Test(2);
+            //MethTest(test);
+            //test.ShowI();
+            //Console.ReadLine();
         }
 
         static void MethTest(ITest test)
