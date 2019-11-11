97813f3670dec437d06a41b99982aabee67ac712
balkar20 <balkar20@mail.ru>
2019-11-11 17:17:47 +0300

    рп

---
+++ NewSharpWork/Deconstruct/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Deconstruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (var myX, var myY) = new Point(4,5);
+            Console.WriteLine($"{myX } && { myY}");
+
+            (var first, var middle, var last) = LookupName(4);
+            Console.WriteLine($"{first} {middle} {last}");
+        }
+
+        static (string, string, string) LookupName(long id) // возвращаемый тип - кортеж
+        {
+    return ("Vlad", "Alex", "Balkarov"); // литерал кортежа
+        }
+    }
+
+    class Point
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Point(int x, int y) { X = x; Y = y; }
+        public void Deconstruct(out int x, out int y) { x = X; y = Y; }
+    }
+}
