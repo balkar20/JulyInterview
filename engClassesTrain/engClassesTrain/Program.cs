2f06a15f0799d59ae3180d2c962a5f94a4ff778e
vbalkarov <vladislav.balkarov@artezio.com>
2019-11-15 17:15:52 +0300

    hghg

---
+++ engClassesTrain/engClassesTrain/Program.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+
+namespace engClassesTrain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repo = new UserRepository();
+
+            // Настройка AutoMapper
+            Mapper.Initialize(cfg => cfg.CreateMap<User, IndexUserViewModel>());
+            Mapper.Map<IEnumerable<User>, List<IndexUserViewModel>>(repo.GetAll());
+            return View(users);
+        }
+    }
+}
---
+++ engClassesTrain/engClassesTrain/UserContext.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace engClassesTrain
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+
+    public class UserRepository : IRepository<User>
+    {
+        UserContext db;
+        public UserRepository()
+        {
+            db = new UserContext();
+        }
+        public IEnumerable<User> GetAll()
+        {
+            return db.Users.ToList();
+        }
+
+        public User Get(int id)
+        {
+            return db.Users.Find(id);
+        }
+
+        public void Create(User item)
+        {
+            db.Users.Add(item);
+        }
+
+        public void Update(User item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+        }
+
+        public void Delete(int id)
+        {
+            User user = db.Users.Find(id);
+            if (user != null)
+                db.Users.Remove(user);
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+    interface IRepository<T> : IDisposable
+        where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
+        void Save();
+    }
+
+}
